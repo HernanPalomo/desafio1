6efc587dc0bd8355bddd80ee7f6a3e800618082b
Hernan Palomo <hdej98@gmail.com>
2022-07-02 22:56:43 -0300

    commit3

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function Home() {
   return (
     <div>
-      <h1>Hola Mundo!!!.</h1>
-      
+      <h1>Hola Mundo!!!</h1>
+      <h1>Hola Mundo!!!</h1>
     </div>
   )
 }
